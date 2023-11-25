afa3d10b9fef9137b145a55dcacffea0e5fd4812
ADMIN <egurevich75@mail.ru>
2023-11-25 13:20:16 +0300

    добавила кнопки да, нет, помощь

--- py_bot/cinema_bot.py
+++ py_bot/cinema_bot.py
@@ -29,8 +29,24 @@ bot = telebot.TeleBot('6918194595:AAHo7WOtM3T6YMR4fk0u229FUlMd5E7rwQw')
 def start(message):
     bot.send_photo(message.chat.id,
                    photo='https://cdn.dribbble.com/users/1436489/screenshots/4653126/bot_bruxelles_loop.gif')
-    bot.send_message(message.chat.id, "Привет! Я бот, который ищет фильмы. Начать поиск?")
-    bot.send_message(message.chat.id, 'Введите Да/Нет')
+    bot.send_message(message.chat.id, "Привет! Я бот, который ищет фильмы.")
+
+    markup_inline = types.InlineKeyboardMarkup()
+    yes = types.InlineKeyboardButton(text='Да', callback_data='yes')
+    no = types.InlineKeyboardButton(text='Нет', callback_data='no')
+    info = types.InlineKeyboardButton(text='помощь', callback_data='info')
+    markup_inline.add(yes, no, info)
+    bot.send_message(message.chat.id, 'Хотите начать поиск?', reply_markup=markup_inline)
+
+@bot.callback_query_handler(func=lambda call:True)
+def answer(call):
+    if call.data == 'yes':
+        ...
+    elif call.data == 'no':
+        ...
+    elif call.data == 'info':
+        bot.send_message(call.from_user.id, 'это справочная информация')
+
 
 @bot.message_handler(content_types=['text'])
 def get_text_message(message):
@@ -50,7 +66,6 @@ def get_text_message(message):
         bot.send_message(message.from_user.id, 'Если Вы хотите продолжить поиск, наберите  "Дa". Для выхода введите "Нет". Если Вы хотите выбрать другой фильм, напишите "другой"')
     else:
         bot.send_message(message.from_user.id, 'Я тебя не понимаю, набери /help.')
-
 
 
 # обработчик нажатой кнопки
