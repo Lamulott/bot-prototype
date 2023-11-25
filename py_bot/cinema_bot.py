b4baa7346980642683dacb2691ffb1761369053b
ADMIN <egurevich75@mail.ru>
2023-11-25 11:35:15 +0300

    добавила возможность прощаться по-разному, подкорректровала сообщения

--- py_bot/cinema_bot.py
+++ py_bot/cinema_bot.py
@@ -6,7 +6,7 @@ import random
 
 
 URL = 'https://www.baskino.re/novinki-v2/'
-def parser(url):
+def parser(url) -> list[str]:
     r = requests.get(URL)
     soup = b(r.text, 'html.parser')
     films = soup.find('div', class_ ='shortpost')
@@ -20,16 +20,21 @@ def parser(url):
 films_for_today = parser(URL)
 random.shuffle(films_for_today)
 
+parting = ['До новых встреч!', 'До встречи!', 'Пока-пока!', 'До скорой встречи!', 'До следующей встречи!', 'See you soon!']
+random.shuffle(parting)
+num = [i for i in range(len(parting))]
+
 bot = telebot.TeleBot('6918194595:AAHo7WOtM3T6YMR4fk0u229FUlMd5E7rwQw')
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_photo(message.chat.id,
                    photo='https://cdn.dribbble.com/users/1436489/screenshots/4653126/bot_bruxelles_loop.gif')
-    sent = bot.send_message(message.chat.id, "Привет! Я бот, который ищет фильмы. Начать поиск? Да/нет")
+    bot.send_message(message.chat.id, "Привет! Я бот, который ищет фильмы. Начать поиск?")
+    bot.send_message(message.chat.id, 'Введите Да/Нет')
 
 @bot.message_handler(content_types=['text'])
 def get_text_message(message):
-    if message.text == 'Да':
+    if message.text.lower() == 'да':
         bot.send_message(message.from_user.id, 'Отлично! Поехали!')
         bot.send_message(message.from_user.id, "Вот что можно посмотреть сегодня:")
         bot.send_message(message.from_user.id, films_for_today[0])
@@ -38,12 +43,15 @@ def get_text_message(message):
         bot.send_message(message.from_user.id, films_for_today[0])
         del films_for_today[0]
 
-    elif message.text == 'нет':
-        bot.send_message(message.from_user.id, 'До новых встреч!')
+    elif message.text.lower() == 'нет' or message.text.lower() == 'выход':
+        n = random.choice(num)
+        bot.send_message(message.from_user.id, parting[n])
     elif message.text == '/help':
-        bot.send_message(message.from_user.id, 'Если Вы хотите продолжить поиск, наберите Д. Для выхода - н. Если Вы хотите выбрать другой фильм, напишите "другой"')
+        bot.send_message(message.from_user.id, 'Если Вы хотите продолжить поиск, наберите  "Дa". Для выхода введите "Нет". Если Вы хотите выбрать другой фильм, напишите "другой"')
     else:
         bot.send_message(message.from_user.id, 'Я тебя не понимаю, набери /help.')
+
+
 
 # обработчик нажатой кнопки
 @bot.callback_query_handler(func=lambda call:True)
