f662086ba001f69094e37b62fe700b8c07e9ea5d
ADMIN <egurevich75@mail.ru>
2023-12-12 16:46:25 +0300

    добавила main()

--- py_bot/cinema_bot.py
+++ py_bot/cinema_bot.py
@@ -4,12 +4,12 @@ import requests
 from bs4 import BeautifulSoup as b
 import random
 
-
 url = 'https://www.baskino.re/novinki-v2/'
 headers = {
     'Accept': '*/*',
     'User_Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.5993.731 Mobile Safari/537.36'
 }
+
 
 def parser(url, headers) -> list[str]:
     r = requests.get(url, headers=headers)
@@ -38,14 +38,14 @@ bot = telebot.TeleBot('6918194595:AAHo7WOtM3T6YMR4fk0u229FUlMd5E7rwQw')
 def start(message):
     photo = open('robo.jpg', 'rb')
     bot.send_photo(message.chat.id, photo)
-    bot.send_message(message.chat.id, "Привет! Я бот, который умеет искать фильмы-новинки!")
+    bot.send_message(message.chat.id, "Привет! Я бот, который умеет искать фильмы!")
 
     markup_inline = types.InlineKeyboardMarkup()
     yes = types.InlineKeyboardButton(text='Да', callback_data='yes')
     no = types.InlineKeyboardButton(text='Нет', callback_data='no')
     info = types.InlineKeyboardButton(text='помощь', callback_data='info')
     markup_inline.add(yes, no, info)
-    bot.send_message(message.chat.id, 'Хотите начать поиск?', reply_markup=markup_inline)
+    bot.send_message(message.chat.id, 'Начать поиск?', reply_markup=markup_inline)
 
 
 @bot.callback_query_handler(func=lambda call: True)
@@ -97,4 +97,8 @@ def get_text_message(message):
         bot.send_message(message.from_user.id, 'Я тебя не понимаю, набери /help.')
 
 
-bot.polling(none_stop=True, interval=0)
+def main():
+    bot.polling(none_stop=True, interval=0)
+
+if __name__ == '__main__':
+    main()
