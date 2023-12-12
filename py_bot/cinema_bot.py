e8d8629ef7849e7888cadc10f80fc5c1676cee9a
ADMIN <egurevich75@mail.ru>
2023-12-12 15:08:13 +0300

    добавила картинку

--- py_bot/cinema_bot.py
+++ py_bot/cinema_bot.py
@@ -4,7 +4,7 @@ import requests
 from bs4 import BeautifulSoup as b
 import random
 
-# url_list = ['https://www.baskino.re/novinki-v2/', 'https://baskino.re/novinki-v2/page/2/']
+
 url = 'https://www.baskino.re/novinki-v2/'
 headers = {
     'Accept': '*/*',
@@ -36,9 +36,9 @@ bot = telebot.TeleBot('6918194595:AAHo7WOtM3T6YMR4fk0u229FUlMd5E7rwQw')
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_photo(message.chat.id,
-                   photo='https://cdn.dribbble.com/users/1436489/screenshots/4653126/bot_bruxelles_loop.gif')
-    bot.send_message(message.chat.id, "Привет! Я бот, который ищет фильмы.")
+    photo = open('robo.jpg', 'rb')
+    bot.send_photo(message.chat.id, photo)
+    bot.send_message(message.chat.id, "Привет! Я бот, который умеет искать фильмы-новинки!")
 
     markup_inline = types.InlineKeyboardMarkup()
     yes = types.InlineKeyboardButton(text='Да', callback_data='yes')
@@ -73,28 +73,20 @@ def get_text_message(message):
     if message.text.lower() == 'да':
         bot.send_message(message.from_user.id, "Вот что можно посмотреть сегодня:")
         bot.send_message(message.from_user.id, films_for_today[0])
+        random.shuffle(films_for_today)
 
-    elif message.text.lower() == 'другой' or message.text.lower() == 'начать':
+    elif message.text.lower() == 'другой' or message.text.lower() == 'продолжить':
         bot.send_message(message.from_user.id, films_for_today[0])
-        del films_for_today[0]
+        random.shuffle(films_for_today)
 
     elif message.text.lower() == 'нет' or message.text.lower() == 'выход':
         n = random.choice(num)
         bot.send_message(message.from_user.id, parting[n])
     elif message.text == '/help':
         bot.send_message(message.from_user.id,
-                         'Для того, чтобы начать поиск, нажмите "Дa". Для выхода нажмите "Нет" или "Выход". Для выбора другого фильма нажмите кнопку "другой". Так же для продолжения поиска Вы можете набрать в чате "Начать". Приятного поиска!')
+                         'Для того, чтобы начать поиск, наберите /start. Для выхода нажмите на кнопку "Выход"; для выбора другого фильма - "Другой". Так же для продолжения поиска Вы можете набрать в чате "Продолжить". Приятного поиска!')
     else:
         bot.send_message(message.from_user.id, 'Я тебя не понимаю, набери /help.')
-
-
-# обработчик нажатой кнопки
-@bot.callback_query_handler(func=lambda call: True)
-def callback_worker(call):
-    if call.data == 'url':
-        msg = 'К сожалению пока этот бот находится на стадии разработки'
-        # отправляем в тг
-        bot.send_message(call.message.chat.id, msg)
 
 
 bot.polling(none_stop=True, interval=0)
